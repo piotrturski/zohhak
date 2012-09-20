87b555c9011488ab8da55a87361935ae81887303
Piotr Turski <piotr.turski@gmail.com>
2012-09-21 01:34:04 +0200

    multiple parameters

--- mintaka/src/main/java/net/piotrturski/mintaka/internal/CoercingService.java
+++ mintaka/src/main/java/net/piotrturski/mintaka/internal/CoercingService.java
@@ -42,7 +42,11 @@ public class CoercingService {
 	}
 	
 	protected String[] split(String input, int expectedArgumentsNumber, Configuration configuration) {
-		return new String[]{input};
+		String[] splited = input.split("[,|]");
+		for (int i = 0; i < splited.length; i++) {
+			splited[i] = splited[i].trim();
+		}
+		return splited;
 	}
 
 }
---
+++ mintaka/src/test/java/net/piotrturski/mintaka/SplittingTest.java
@@ -0,0 +1,44 @@
+package net.piotrturski.mintaka;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+import net.piotrturski.mintaka.helper.SampleEnum;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+@RunWith(MintakaRunner.class)
+public class SplittingTest {
+
+	@TestWith("7, 19")
+	public void twoParams(int i, int j) {
+		assertThat(i).isEqualTo(7);
+		assertThat(j).isEqualTo(19);
+	}
+
+	@TestWith("7, LAST_VALUE")
+	public void differentParamTypes(int i, SampleEnum j) {
+		assertThat(i).isEqualTo(7);
+		assertThat(j).isEqualTo(SampleEnum.LAST_VALUE);
+	}
+	
+	@TestWith("7 | 19")
+	public void otherSeparator(int i, int j) {
+		assertThat(i).isEqualTo(7);
+		assertThat(j).isEqualTo(19);
+	}
+	
+	@TestWith("7 | 19, 23")
+	public void mixedSeparators(int i, int j, int k) {
+		assertThat(i).isEqualTo(7);
+		assertThat(j).isEqualTo(19);
+		assertThat(k).isEqualTo(23);
+	}
+	
+	@TestWith("1")
+	@Ignore
+	public void varargs1(int... ints) {
+		assertThat(ints).startsWith(1).hasSize(1);
+	}
+	
+}
