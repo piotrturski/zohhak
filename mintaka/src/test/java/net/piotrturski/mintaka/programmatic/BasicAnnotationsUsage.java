2e6db0eb890ba83235fcc7947959ce6f3bcfffc4
Piotr Turski <piotr.turski@gmail.com>
2012-09-27 02:18:29 +0200

    hack for making failing tests pass when run directly from under an IDE

--- mintaka/src/test/java/net/piotrturski/mintaka/programmatic/BasicAnnotationsUsage.java
+++ mintaka/src/test/java/net/piotrturski/mintaka/programmatic/BasicAnnotationsUsage.java
@@ -1,8 +1,14 @@
 package net.piotrturski.mintaka.programmatic;
 
 import static org.junit.Assert.fail;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import net.piotrturski.mintaka.MintakaRunner;
+import net.piotrturski.mintaka.ProgrammaticRunTest;
 import net.piotrturski.mintaka.TestWith;
+import net.piotrturski.mintaka.ide.EclipseIntegration;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -10,8 +16,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * This test is not supposed to be run on its own (under IDE or maven). in such case it will fail. 
- * it's used by other tests to check
+ * This test is not supposed to be run on its own (under IDE or maven). 
+ * it's hacked not to fail in such case (just for convenience). it has no value when run directly from under an IDE.
+ *  
+ * this class is used by other tests to check
  * if integration with JUnit (annotation discovery, method invocation, reporting etc.) is working properly.
  *
  */
@@ -25,7 +33,7 @@ public class BasicAnnotationsUsage {
 	@Test public void methodToRun() {}
 	
 	@Test public void method3() {
-		fail();
+		failFrom(EclipseIntegration.class, ProgrammaticRunTest.class);
 	}
 	
 	@TestWith({"1", "2"})
@@ -34,5 +42,25 @@ public class BasicAnnotationsUsage {
 	@Test @Ignore public void ignored() {} 
 	
 	@Ignore @TestWith({"2", "1"})
-	public void parametrizedIgnored(int i) {} 
+	public void parametrizedIgnored(int i) {}
+	
+	
+	
+	/**
+	 * this method causes test to fail but only when invoked by specific classes. it prevents this class from failing when
+	 * run directly from under an IDE (together with all other test classes).
+	 * 
+	 * @param classes
+	 */
+	private void failFrom(Class<?>... classes) {
+		Set<String> classesOnStackTrace = new HashSet<String>();
+		for (StackTraceElement s : Thread.currentThread().getStackTrace()) {
+			classesOnStackTrace.add(s.getClassName());
+		}
+		for (Class<?> clazz : classes) {
+			if (classesOnStackTrace.contains(clazz.getCanonicalName())) {
+				fail();
+			}
+		}
+	}
 }
