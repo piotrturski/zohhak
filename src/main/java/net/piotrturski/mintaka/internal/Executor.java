f3b0f3058a3e29027feb5d74b79c3869f8907d49
Piotr Turski <piotr.turski@gmail.com>
2012-10-06 18:04:39 +0200

    moving classes for better design

--- src/main/java/net/piotrturski/mintaka/internal/Executor.java
+++ src/main/java/net/piotrturski/mintaka/internal/Executor.java
@@ -2,7 +2,6 @@ package net.piotrturski.mintaka.internal;
 
 import net.piotrturski.mintaka.Configuration;
 import net.piotrturski.mintaka.internal.coercing.CoercingService;
-import net.piotrturski.mintaka.internal.coercing.ConfigurationResolver;
 
 public final class Executor {
 
--- src/main/java/net/piotrturski/mintaka/internal/IoCContainer.java
+++ src/main/java/net/piotrturski/mintaka/internal/IoCContainer.java
@@ -2,7 +2,6 @@ package net.piotrturski.mintaka.internal;
 
 import net.piotrturski.mintaka.internal.coercing.Cache;
 import net.piotrturski.mintaka.internal.coercing.CoercingService;
-import net.piotrturski.mintaka.internal.coercing.ConfigurationResolver;
 
 public class IoCContainer {
 
