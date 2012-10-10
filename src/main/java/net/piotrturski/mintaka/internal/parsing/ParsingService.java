79a042fbe96327736d0467d823d52d525bd7c4cb
Piotr Turski <piotr.turski@gmail.com>
2012-10-11 01:06:02 +0200

    refactoring

--- src/main/java/net/piotrturski/mintaka/internal/parsing/ParsingService.java
+++ src/main/java/net/piotrturski/mintaka/internal/parsing/ParsingService.java
@@ -4,27 +4,24 @@ import net.piotrturski.mintaka.internal.model.SingleTestMethod;
 
 public class ParsingService {
 
-	private static final String CANNOT_PARSE_PARAMETERS = "Cannot parse parameters";
 	private Parser parser = new Parser();//TODO export as a configuration parameter?
 	
 	public String[] split(SingleTestMethod method) {
 		String[] splitedParameters;
 		
 		try {
+			
 			splitedParameters = parser.split(method);
 			
-		} catch (RuntimeException e) {
-			throw new IllegalArgumentException(CANNOT_PARSE_PARAMETERS, e);
-		} catch (AssertionError e) {
-			throw new IllegalArgumentException(CANNOT_PARSE_PARAMETERS, e);
+		} catch (Throwable t) {
+			throw new IllegalArgumentException("Cannot parse parameters", t);
 		}
 		
-		int numberOfDeclaredParameters = method.realMethod.getGenericParameterTypes().length;
+		int numberOfDeclaredParameters = method.getArity();
 		if (splitedParameters.length != numberOfDeclaredParameters) {
 			throw new IllegalArgumentException(numberOfDeclaredParameters+" parameter(s) declared but provided "+splitedParameters.length);
 		}
 		return splitedParameters;
-		
 	}
 	
 }
