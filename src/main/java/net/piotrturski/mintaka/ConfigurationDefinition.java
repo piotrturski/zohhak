bb5af02b16f4ef4ef7a3c7ee5f5bac088eb1904e
Piotr Turski <piotr.turski@gmail.com>
2012-10-17 01:22:07 +0200

    cleanup for sonar

--- src/main/java/net/piotrturski/mintaka/ConfigurationDefinition.java
+++ src/main/java/net/piotrturski/mintaka/ConfigurationDefinition.java
@@ -2,9 +2,9 @@ package net.piotrturski.mintaka;
 
 public interface ConfigurationDefinition {
 
-	final String DEFAULT_SEPARATOR_MARKER = "\u0000";
-	final String DEFAULT_SEPARATOR = ",";
+	String DEFAULT_SEPARATOR_MARKER = "\u0000";
+	String DEFAULT_SEPARATOR = ",";
 
-	final String DEFAULT_STRING_BOUNDARY_MARKER = "\u0000";
-	final String DEFAULT_STRING_BOUNDARY = "'";
+	String DEFAULT_STRING_BOUNDARY_MARKER = "\u0000";
+	String DEFAULT_STRING_BOUNDARY = "'";
 }
