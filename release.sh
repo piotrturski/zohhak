d793188d00361730dfa69f6911cf635fe71213d6
Piotr Turski <piotr.turski@gmail.com>
2015-01-05 21:03:21 +0100

    better release automation

--- release.sh
+++ release.sh
@@ -1,7 +1,4 @@
-mvn versions:use-latest-releases
-
-
-mvn versions:set 
+mvn versions:set
 # -DnewVersion=1.0.2
-mvn verify source:jar javadoc:jar gpg:sign deploy
+mvn clean verify source:jar javadoc:jar gpg:sign deploy
 mvn versions:revert
